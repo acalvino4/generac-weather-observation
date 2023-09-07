d14a28ef2f25669c2a95416349c8ad3cb94572f3
acalvino4 <acalvino4@gmail.com>
2023-09-07 15:22:25 -0600

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line n/file-extension-in-import
+import {sveltekit} from '@sveltejs/kit/vite';
+// eslint-disable-next-line n/file-extension-in-import
+import {defineConfig} from 'vitest/config';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import okLab from '@csstools/postcss-oklab-function';
+import type {Plugin} from 'postcss';
+import tailwindConfig from './tailwind.config.js';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+	css: {
+		postcss: {
+			plugins: [tailwindcss(tailwindConfig) as Plugin, autoprefixer(), okLab()],
+		},
+	},
+});
